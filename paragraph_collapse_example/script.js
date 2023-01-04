55d49f50fd38045438e6c7324dc812be3b182000
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-04 10:22:12 +0700

    a simple faq page by Ngan

---
+++ paragraph_collapse_example/script.js
@@ -0,0 +1,18 @@
+// $$:  query selector all
+
+const $$ = document.querySelectorAll.bind(document);
+// const questions = $$("#faq .content-container ul li span");
+const questions = $$(".question");
+const answers = $$(".answer");
+for (let i = 0; i < questions.length; i++)
+{
+    questions[i].addEventListener("click", function() {
+        //this = question[i]
+       
+        // this.parentElement.classList.toggle("active");
+        
+        this.children[0].classList.toggle("fa-caret-down");
+        this.children[0].classList.toggle("fa-caret-up");
+        answers[i].classList.toggle("active");
+    })
+}
