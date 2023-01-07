b6d239c48593e1f5f7075559693460260b2613c6
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-07 14:46:16 +0700

    fix bug of button

--- js-app-example-ngan/todo-app-example-ngan/script.js
+++ js-app-example-ngan/todo-app-example-ngan/script.js
@@ -1,68 +1,119 @@
 
-const $$ = document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
 
-const taskField = $$(".input-field");
-const addTaskBtn = $$(".addTask");
-const taskList = $$(".task-list");
-var tasks = [];
-let TaskArrayLC = [];
-let li;
-let completeBtn, removeBtn, containerBtn;
- completeBtn = document.createElement("button");
-    removeBtn = document.createElement("button");
-    containerBtn = document.createElement("div");
-
-addTaskBtn.addEventListener("click", AddTask);
+const taskField = $(".input-field");
+const addTaskBtn = $(".addTask");
+const taskList = $(".task-list");
+var tasksArrayFromInput = [];
+let taskArrayFromLS = [];
+let task ;
+addTaskBtn.addEventListener("click", validateTask);
 window.onload = function (e) {
-    const myTasks = getAllFromLC();
-  
-    
-}
-
- 
-function CreateTaskElement (TaskValue) {
-    tasks.push({
-        task: TaskValue,
-        isComplete: false,
-    })
-    saveLC(tasks);
-    li = document.createElement("li");
-    const p = document.createElement("p");
-    if (TaskValue.toString().length  > 0)
-    {
-        p.innerText = TaskValue;
+    const myTasks = getAllFromLS();
+    if (Array.isArray(myTasks)) {
+        for (let i = 0; i < myTasks.length ; i++) {
+            CreateTaskElement(myTasks[i].task);
+            CreateButton(e);
+        }
     }
+};
 
-    li.appendChild(p);
-    CreateButton();  
-    taskList.appendChild(li);
 
-     
+//function
+//function Save to localstorage
+function saveToLC(tasks) {
+    localStorage.setItem("task", JSON.stringify(tasks));
 }
-function CreateButton () {
-    completeBtn = document.createElement("button");
-    removeBtn = document.createElement("button");
-    containerBtn = document.createElement("div");
+function getAllFromLS() {
+    let myTask = localStorage.getItem("task");
+    taskArrayFromLS = JSON.parse(myTask);
+    return taskArrayFromLS;
+}
+
+//validateTask 
+function validateTask (e) {
+    e.preventDefault();
+    let isEmptyString = checkEmptyValue(taskField.value);
+    if (isEmptyString) {
+        AddTask();
+        saveToLC(tasksArrayFromInput);
+    }
+    else {
+        alert("Please enter your task!");
+    }
+}
+function checkEmptyValue(value)
+{
+    if (value === "") {
+        return false;
+      } else {
+        return true;
+      }
+}
+ //Handle AddTask
+function AddTask (e) {
+    CreateTaskElement(taskField.value);
+    CreateButton(e);
+  
+    //Reset task field
+    taskField.value = "";
+    //Re-focus task field   
+    taskField.focus();
+ 
+}
+function CreateTaskElement (taskName) {
+
+     task = document.createElement("li");
+    task.style.cssText = `
+    padding: .5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+     `;
+     task.innerHTML = `<p>${taskName}</p>`;
+    
+     //push task to local storage array
+    tasksArrayFromInput.push({
+        task: task.textContent,
+        isCompleted: false,
+    });
+    taskList.appendChild(task);
+}
+function CreateButton (e) {
+
+    //Create div element to contain 2 button
+    const containerBtn = document.createElement("div");
+    containerBtn.classList.add("containerBtn");
+
+    //Create 2 button: Complete and Remove
+    //Button Complete
+    const completeBtn = document.createElement("button");
+    completeBtn.classList.add("completeBtn");
     completeBtn.innerHTML = `<i class="fa-regular fa-circle-check completeBtn"></i>`;
-    completeBtn.classList.add("complete");
+ 
+    //Button Remove
+    const removeBtn = document.createElement("button");
     removeBtn.innerHTML = `<i class="fa-solid fa-circle-xmark removeBtn"></i>`
-    removeBtn.classList.add("cancel")
+    removeBtn.classList.add("removeBtn")
+
+    //Append 2 button to container
     containerBtn.appendChild(completeBtn);
     containerBtn.appendChild(removeBtn);
-    li.appendChild(containerBtn);
-}
-//Handle AddTask
-function AddTask (e) {
-    let task = taskField.value;
-    CreateTaskElement(task);
-    e.preventDefault(); 
-   
-    removeBtn.onclick = function (e) {
-      
+    task.appendChild(containerBtn);
+
+    //handle complete task
+    completeBtn.onclick = function (e) {
+        const taskElement = this.parentElement.parentElement.childNodes[0] ;
+        e.preventDefault();
+        completeTask(taskElement);
+    }
+
+    //handle remove task
+   removeBtn.onclick = function (e) {
         const taskElement = this.parentElement.parentElement;
+        e.preventDefault();
         removeTask(taskElement);
-        e.preventDefault(); 
-    } 
+    };
 }
 
 function completeTask (taskElement) {
@@ -72,83 +123,4 @@ function removeTask(taskElement) {
     taskElement.remove();   
 }
 
-//function Save to localstorage
-function saveLC(tasks) {
-    localStorage.setItem("task", JSON.stringify(tasks));
-}
-function getAllFromLC() {
-    const str = localStorage.getItem("task");
-    TaskArrayLC = JSON.parse(str);
-     for(let i =0; i< TaskArrayLC.length; i++){
-     
-   
-        CreateTaskElement(TaskArrayLC[i].task);
-    }
 
-    // console.log(completeBtn);
-    completeBtn.addEventListener("click", function(e) {
-        e.preventDefault(); 
-        console.log(completeBtn);
-    })
-    //  completeBtn.onclick = function (e) {
-    //     e.preventDefault(); 
-    //     const taskElement =this.parentElement.parentElement.childNodes[0] ;
-    //     console.log(taskElement);
-    //     // completeTask(taskElement);
-       
-    
-    // }
-
-    // completeBtn.addEventListener("click", function (e) {
-    //     e.preventDefault(); 
-    //     console.log("ds");
-    // });
-}
-function removeLC(taskElement) {
-    localStorage.removeItem("tasks");
-}
-
-// function updateTask(li) {
-
-//     //console.log(taskList);
-
-//     // const tasks = document.querySelectorAll(".task-list li");
-  
-//     console.log(li);
-
-//     // completeBtn.onClick = function () {
-//     //     e.preventDefault(); 
-//     //     const taskElement = this.parentElement.parentElement;
-//     //     console.log(taskElement);
-//     // }
-//     // const cBtn = document.querySelectorAll(".completeBtn");
-
-//     // console.log(tasks);
-//     // for (let i = 0; i < cBtn.length; i++)
-//     // {
-//         // console.log(completeBtn[i]);
-  
-
-//         // cBtn.addEventListener("click", function(e) {
-//         //     console.log(this.tasks);
-//         //    e.preventDefault(); 
-//         // //     console.log("nên")
-//         // // console.log(this.tasks[i].innerText);
-//         // });
-    
-//     // }
-// }
-
-// const cBtn = document.querySelectorAll(".completeBtn");
-// console.log(cBtn);
-// // for (let i = 0; i < cBtn.length; i++)
-    
-// // {
-
-// //     cBtn.addEventListener("click", function(e) {
-
-// //         const taskElement = this.parentElement.parentElement;
-// //         console.log(taskElement);
-// //         e.preventDefault(); 
-// // })
-// // }
