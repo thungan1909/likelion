19701abadd28b2f6d18436c4ff933ff3222317a8
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-14 10:16:36 +0700

    slider-project

---
+++ slider_project/index.js
@@ -0,0 +1,8 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const sliderNow = $(".slider-now");
+
+const imgPreview = $(".img-prev");
+
+
